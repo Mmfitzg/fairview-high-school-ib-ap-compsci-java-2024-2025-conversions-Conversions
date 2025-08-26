d432e826d6c855245bfb358e2b8a8ad879ddb85c
Mmfitzg <mmfitzgerrell01@bvsd.org>
2025-08-26 20:46:07 +0000

    finished project

--- Conversions.java
+++ Conversions.java
@@ -27,7 +27,7 @@ public class Conversions
         int selection = keyboard.nextInt();
         keyboard.nextLine();  //go past the endline and be ready for more input
 
-        if (selection == 1)
+        if (selection == 0)
         {
             System.out.println("Enter Celsius: ");
             double celsius = keyboard.nextDouble();
@@ -35,7 +35,7 @@ public class Conversions
             double fahrenheit = celsius * (9.0 / 5.0) + 32;
             System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
         }
-        if (selection == 2)
+        if (selection == 1)
         {
             System.out.println("Enter Fahrenheit: ");
             double fahrenheit2 = keyboard.nextDouble();
@@ -43,7 +43,7 @@ public class Conversions
             double celsius2 = (fahrenheit2 - 32) * 9.0 / 5.0;
             System.out.println(fahrenheit2 + " degrees Fahrenheit is " + celsius2 + " degrees celsius");
         }
-        if (selection == 3)
+        if (selection == 2)
         {
             System.out.println("Enter Feet: ");
             double feet = keyboard.nextDouble();
@@ -51,7 +51,7 @@ public class Conversions
             double meters = feet/3.281;
             System.out.println(feet + " Feet is " + meters + " Meters");
         }
-        if (selection == 4)
+        if (selection == 3)
         {
             System.out.println("Enter Meters: ");
             double meters2 = keyboard.nextDouble();
@@ -59,7 +59,7 @@ public class Conversions
             double feet2 = meters2 * 3.281;
             System.out.println(meters2 + " Meters is " + feet2 + " Feet");
         }
-        if (selection == 5)
+        if (selection == 4)
         {
             System.out.println("Enter Ounces: ");
             double ounces = keyboard.nextDouble();
@@ -67,7 +67,7 @@ public class Conversions
             double milliliters = ounces * 29.574;
             System.out.println(ounces + " Ounces is " + milliliters + " Milliliters");
         }
-        if (selection == 6)
+        if (selection == 5)
         {
             System.out.println("Enter Milliliters: ");
             double milliliters2 = keyboard.nextDouble();
@@ -75,6 +75,37 @@ public class Conversions
             double ounces2 = milliliters2/29.574;
             System.out.println(milliliters2 + " Milliliters is " + ounces2 + " Ounces");
         }
-
+        if (selection == 6)
+        {   
+            System.out.println("Enter Kilometers: ");
+            double kilometers = keyboard.nextDouble();
+            keyboard.nextLine();
+            double miles = kilometers/1.609;
+            System.out.println(kilometers + " Kilometers is " + miles + " Miles");
+        }
+        if (selection == 7)
+        {   
+            System.out.println("Enter Miles: ");
+            double miles2 = keyboard.nextDouble();
+            keyboard.nextLine();
+            double kilometers2 = miles2 * 1.609;
+            System.out.println(mile2 + " miles is " + kilometers2 + " kilometers");
+        }
+        if (selection == 8)
+        {   
+            System.out.println("Enter Knots: ");
+            double knots = keyboard.nextDouble();
+            keyboard.nextLine();
+            double mph = knots * 1.151;
+            System.out.println(knots + " knots is " + mph + " MPH");
+        }
+        if (selection == 9)
+        {   
+            System.out.println("Enter MPH: ");
+            double mph2 = keyboard.nextDouble();
+            keyboard.nextLine();
+            double knots2 = mph2 / 1.151;
+            System.out.println(mph2 + " MPH is " + knots2 + " knots");
+        }
     }
 }
